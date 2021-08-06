d0b588c51e9da15184a64bfa5ea0327681b01114
Miguel A. Buritica O <mburitica@cafetosoftware.com>
2021-08-06 15:38:29 -0500

    CFS-28: List of posts in chronological order.

--- src/archive.php
+++ src/archive.php
@@ -1,12 +1,26 @@
 <?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @package driven_properties_theme
+ */
 
 get_header();
 
 if ( class_exists( 'Timber' ) ) {
 
-	$context            = Timber::context();
-	$context[ 'posts' ] = new Timber\PostQuery();
-	Timber::render( './view/archive.twig', $context );
+	$populars_posts_args = array(
+		'posts_per_page' => 9,
+		'meta_key'       => 'post_views',
+		'orderby'        => 'meta_value_num',
+		'order'          => 'DESC'
+	);
+
+	$context                    = Timber::context();
+	$context[ 'posts' ]         = new Timber\PostQuery();
+	$context[ 'popularsPosts' ] = Timber::get_posts( $populars_posts_args );
+	Timber::render( './view/archives/blog.twig', $context );
 
 } else {
 	echo '<h1>Timber plugin is required</h1>';
