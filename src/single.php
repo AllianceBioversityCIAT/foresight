8e6fe28904e156bbcea8dfc4a7a21a85ee26a8c7
Miguel A. Buritica O <mburitica@cafetosoftware.com>
2021-08-09 11:59:42 -0500

    CFS-37: Show related post.

--- src/single.php
+++ src/single.php
@@ -2,32 +2,41 @@
 /**
  * The template for displaying all single posts
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
+ * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  * @package foresight
  */
 
 get_header();
 
 if ( class_exists( 'Timber' ) ) {
-	$next_post    = get_next_post();
-	$prev_post    = get_previous_post();
-	$context = Timber::context();
-	$context[ 'post' ] = new Timber\Post();
+	global $wp;
+	$category       = get_the_terms( get_the_ID(), 'category' );
+	$url_components = add_query_arg( $wp->query_vars, home_url( $wp->request ) );
+	parse_str( $url_components, $params );
+	$category_name = $params[ 'category_name' ];
 
-	if ( is_object( $prev_post ) ) {
-		$context[ 'prev_post' ]    = [
-			'permalink' => get_permalink( $prev_post->ID ),
-			'title'     => $prev_post->post_title,
-		];
+	if ( !$params[ 'category_name' ] ) {
+		$category_name = $category[ 0 ]->slug;
 	}
 
-	if ( is_object( $next_post ) ) {
-		$context[ 'next_post' ] = [
-			'permalink' => get_permalink( $next_post->ID ),
-			'title'     => $next_post->post_title,
-		];
-	}
+	$args_related_posts = array(
+		'post_type'      => 'post',
+		'posts_per_page' => 4,
+		'post__not_in'   => [
+			get_the_ID(),
+		],
+		'tax_query'      => [
+			[
+				'taxonomy' => 'category',
+				'field'    => 'slug',
+				'terms'    => $category_name,
+			],
+		],
+	);
+
+	$context                   = Timber::context();
+	$context[ 'post' ]         = new Timber\Post();
+	$context[ 'relatedPosts' ] = Timber::get_posts( $args_related_posts );
 
 	Timber::render( './view/single/single.twig', $context );
 
