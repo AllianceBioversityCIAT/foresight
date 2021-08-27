002c8f7fd9a1d9e2136cb885d1f397d4f9c5f8db
Juan Lora <jlora@cafetosoftware.com>
2021-08-27 08:31:54 -0500

    CFS-46 IMPORTING CONTENT FROM ZOTERO - Save obtained documents as custom post type

--- src/static/js-admin/acf-settings-page.js
+++ src/static/js-admin/acf-settings-page.js
@@ -1,5 +1,8 @@
 jQuery( document ).ready(function($) {
 
+    /**
+     * Import control lists from Clarisa  
+    **/    
 	  $( "#acf-field_import_list" ).on( "click", function() {
 
         $( "#spinner_acf-field_import_list" ).addClass( "is-active" );
@@ -8,7 +11,7 @@ jQuery( document ).ready(function($) {
 		$.ajax({
             type: "post",
             url: ajax_var.url,
-            data: "action=" + ajax_var.action + "&nonce=" + ajax_var.nonce,
+            data: "action=" + ajax_var.action_import_clarisa + "&nonce=" + ajax_var.nonce,
             success: function(result){
                 $( "#acf-field_import_list" ).val("Ready!");
                 location.reload();
@@ -23,5 +26,33 @@ jQuery( document ).ready(function($) {
               }
         });
 	  });
+
+
+     /**
+      * Import items from Zotero
+     **/
+      $( "#acf-field_import_zotero" ).on( "click", function() {
+
+        $( "#spinner_acf-field_import_zotero" ).addClass( "is-active" );
+        $( "#acf-field_import_zotero" ).addClass( "disabled" ).val("Processing...").off( "click" );
+
+		$.ajax({
+            type: "post",
+            url: ajax_var.url,
+            data: "action=" + ajax_var.action_import_zotero + "&nonce=" + ajax_var.nonce,
+            success: function(result){
+                $( "#acf-field_import_zotero" ).val("Ready!");
+                location.reload();
+            },
+            error: function (xhr) {
+                var json_error = JSON.parse(xhr.responseText);
+                if(json_error.success == false){
+                    $( "#acf-field_import_zotero" ).val("Import failed!");
+                    $( "#spinner_acf-field_import_zotero" ).removeClass( "is-active" );
+                    alert(json_error.data[0].code + ": " + json_error.data[0].message);
+                }
+              }
+        });
+	  }); 
 
 });
