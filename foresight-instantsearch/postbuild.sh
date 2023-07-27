5d08a4ab66834dd78b290fe54ccc2c11736c073b
Hemmder Alvarez <galvarez@cafetosoftware.com>
2023-07-27 18:18:28 -0500

    ci(devops): debug script execution

--- foresight-instantsearch/postbuild.sh
+++ foresight-instantsearch/postbuild.sh
@@ -2,12 +2,12 @@
 absolute_path=($(jq -r '.dest' config.json))
 echo $absolute_path
 mkdir -p $absolute_path/algolia
-mv -f ./dist/app.mobile.*.css $absolute_path/algolia/app.mobile.css
-mv -f ./dist/app.desktop.*.css $absolute_path/algolia/app.desktop.css
-mv -f ./dist/theme.*.css $absolute_path/algolia/theme.css
-mv -f ./dist/year-slider.*.css $absolute_path/algolia/year-slider.css
-mv -f ./dist/app.*.js $absolute_path/algolia/app.js
-mv -f ./dist/cover-desktop.*.jpg $absolute_path/algolia/
-mv -f ./dist/cover-mobile.*.jpg $absolute_path/algolia/
-mv -f ./dist/favicon.*.png $absolute_path/algolia/favicon.png
-cp -R ./src/images/sdg/ $absolute_path/algolia/sdg
+# mv -f ./dist/app.mobile.*.css $absolute_path/algolia/app.mobile.css
+# mv -f ./dist/app.desktop.*.css $absolute_path/algolia/app.desktop.css
+# mv -f ./dist/theme.*.css $absolute_path/algolia/theme.css
+# mv -f ./dist/year-slider.*.css $absolute_path/algolia/year-slider.css
+# mv -f ./dist/app.*.js $absolute_path/algolia/app.js
+# mv -f ./dist/cover-desktop.*.jpg $absolute_path/algolia/
+# mv -f ./dist/cover-mobile.*.jpg $absolute_path/algolia/
+# mv -f ./dist/favicon.*.png $absolute_path/algolia/favicon.png
+# cp -R ./src/images/sdg/ $absolute_path/algolia/sdg
